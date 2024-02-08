8506df0ae42b2ddd4b9f11feb6101af7b27bd784
Alexandrina <poidaalesya382@gmail.com>
2024-02-08 18:04:50 +0300

    feat: deleting a task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
@@ -19,5 +19,10 @@ export class TasksController {
   @Post()
   createTask(@Body() createTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
   }
 }
